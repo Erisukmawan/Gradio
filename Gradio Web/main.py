09366719f92e84276bd4dce8ca591a072cc69520
Erisukmawan <erisukmawan03@gmail.com>
2023-02-01 12:18:09 +0700

    Gradio Web

---
+++ Gradio Web/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from routes.dashboard import (
+    main_dashboard,
+    research,
+    scraper
+)
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='./static/',
+            template_folder='./template/')
+
+app.add_url_rule('/', 'index', main_dashboard)
+app.add_url_rule('/research', '', research)
+app.add_url_rule('/scraper', 'scraper', scraper)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
---
+++ Gradio Web/routes/dashboard.py
@@ -0,0 +1,10 @@
+from flask import render_template
+
+def main_dashboard():
+    return render_template('pages/iklim/index.html')
+
+def research():
+    return render_template('pages/penelitian/index.html')
+
+def scraper():
+    return render_template('pages/scraper/index.html')
