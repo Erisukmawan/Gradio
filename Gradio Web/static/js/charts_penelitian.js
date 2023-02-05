0e53b127dc501aea45d57275079024611e582666
Erisukmawan <erisukmawan03@gmail.com>
2023-02-05 19:55:14 +0700

    penelitian part 3

--- Gradio Web/static/js/charts_penelitian.js
+++ Gradio Web/static/js/charts_penelitian.js
@@ -624,3 +624,291 @@ am5.ready(function () {
     chart.appear(1000, 100);
 
 }); // end am5.ready()
+
+am5.ready(function () {
+
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new("perkembanganketinggianlaut");
+
+
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+        am5themes_Animated.new(root)
+    ]);
+
+
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/
+    var chart = root.container.children.push(am5xy.XYChart.new(root, {
+        panX: true,
+        panY: true,
+        wheelX: "panX",
+        wheelY: "zoomX",
+        pinchZoomX: true
+    }));
+
+    // Add cursor
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+    var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+        behavior: "none"
+    }));
+    cursor.lineY.set("visible", false);
+
+
+    // Generate random data
+    var date = new Date();
+    date.setHours(0, 0, 0, 0);
+    var value = 100;
+
+    function generateData() {
+        value = Math.round((Math.random() * 10 - 5) + value);
+        am5.time.add(date, "day", 1);
+        return {
+            date: date.getTime(),
+            value: value
+        };
+    }
+
+    function generateDatas(count) {
+        var data = [];
+        for (var i = 0; i < count; ++i) {
+            data.push(generateData());
+        }
+        return data;
+    }
+
+
+    // Create axes
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+    var xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
+        maxDeviation: 0.5,
+        baseInterval: {
+            timeUnit: "day",
+            count: 1
+        },
+        renderer: am5xy.AxisRendererX.new(root, {
+            pan: "zoom"
+        }),
+        tooltip: am5.Tooltip.new(root, {})
+    }));
+
+    var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+        maxDeviation: 1,
+        renderer: am5xy.AxisRendererY.new(root, {
+            pan: "zoom"
+        })
+    }));
+
+
+    // Add series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+    var series = chart.series.push(am5xy.SmoothedXLineSeries.new(root, {
+        name: "Series",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value",
+        valueXField: "date",
+        tooltip: am5.Tooltip.new(root, {
+            labelText: "{valueY}"
+        })
+    }));
+
+    series.fills.template.setAll({
+        visible: true,
+        fillOpacity: 0.2
+    });
+
+    series.bullets.push(function () {
+        return am5.Bullet.new(root, {
+            locationY: 0,
+            sprite: am5.Circle.new(root, {
+                radius: 4,
+                stroke: root.interfaceColors.get("background"),
+                strokeWidth: 2,
+                fill: series.get("fill")
+            })
+        });
+    });
+
+
+    // Add scrollbar
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+    chart.set("scrollbarX", am5.Scrollbar.new(root, {
+        orientation: "horizontal"
+    }));
+
+
+    var data = generateDatas(50);
+    series.data.setAll(data);
+
+
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/
+    series.appear(1000);
+    chart.appear(1000, 100);
+
+});
+
+//prediksi
+am5.ready(function () {
+
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new("prediksilaut");
+
+
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+        am5themes_Animated.new(root)
+    ]);
+
+
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/
+    var chart = root.container.children.push(am5xy.XYChart.new(root, {
+        panX: true,
+        panY: true,
+        wheelX: "panX",
+        wheelY: "zoomX",
+        pinchZoomX: true
+    }));
+
+    chart.get("colors").set("step", 3);
+
+
+    // Add cursor
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+    var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+    cursor.lineY.set("visible", false);
+
+
+    // Create axes
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+    var xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
+        maxDeviation: 0.3,
+        baseInterval: {
+            timeUnit: "day",
+            count: 1
+        },
+        renderer: am5xy.AxisRendererX.new(root, {}),
+        tooltip: am5.Tooltip.new(root, {})
+    }));
+
+    var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+        maxDeviation: 0.3,
+        renderer: am5xy.AxisRendererY.new(root, {})
+    }));
+
+
+    // Add series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+    var series = chart.series.push(am5xy.LineSeries.new(root, {
+        name: "Series 1",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value1",
+        valueXField: "date",
+        tooltip: am5.Tooltip.new(root, {
+            labelText: "{valueX}: {valueY}\n{previousDate}: {value2}"
+        })
+    }));
+
+    series.strokes.template.setAll({
+        strokeWidth: 2
+    });
+
+    series.get("tooltip").get("background").set("fillOpacity", 0.5);
+
+    var series2 = chart.series.push(am5xy.LineSeries.new(root, {
+        name: "Series 2",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value2",
+        valueXField: "date"
+    }));
+    series2.strokes.template.setAll({
+        strokeDasharray: [2, 2],
+        strokeWidth: 2
+    });
+
+    var series3 = chart.series.push(am5xy.LineSeries.new(root, {
+        name: "Series 3",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value3",
+        valueXField: "date"
+    }));
+    series3.strokes.template.setAll({
+        strokeDasharray: [2, 2],
+        strokeWidth: 2
+    });
+
+    // Set date fields
+    // https://www.amcharts.com/docs/v5/concepts/data/#Parsing_dates
+    root.dateFormatter.setAll({
+        dateFormat: "yyyy-MM-dd",
+        dateFields: ["valueX"]
+    });
+
+
+    // Set data
+    var data = [{
+        date: new Date(2019, 5, 12).getTime(),
+        value1: 50,
+        value2: 48,
+        value3: 58,
+        previousDate: new Date(2019, 5, 5)
+    }, {
+        date: new Date(2019, 5, 13).getTime(),
+        value1: 53,
+        value2: 51,
+        value3: 68,
+        previousDate: "2019-05-06"
+    }, {
+        date: new Date(2019, 5, 14).getTime(),
+        value1: 56,
+        value2: 58,
+        value3: 58,
+        previousDate: "2019-05-07"
+    }, {
+        date: new Date(2019, 5, 15).getTime(),
+        value1: 52,
+        value2: 53,
+        value3: 65,
+        previousDate: "2019-05-08"
+    }, {
+        date: new Date(2019, 5, 16).getTime(),
+        value1: 48,
+        value2: 44,
+        value3: 48,
+        previousDate: "2019-05-09"
+    }, {
+        date: new Date(2019, 5, 17).getTime(),
+        value1: 47,
+        value2: 42,
+        value3: 68,
+        previousDate: "2019-05-10"
+    }, {
+        date: new Date(2019, 5, 18).getTime(),
+        value1: 59,
+        value2: 55,
+        value3: 78,
+        previousDate: "2019-05-11"
+    }]
+
+    series.data.setAll(data);
+    series2.data.setAll(data);
+    series3.data.setAll(data);
+
+    makeSeries("Ketinggian", "value1");
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/
+    series.appear(1000);
+    series2.appear(1000);
+    series3.appear(1000);
+    chart.appear(1000, 100);
+
+});
